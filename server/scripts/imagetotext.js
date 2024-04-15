4b92519e4405c0e05d747d42ccd1b3db65f5841e
Captain Jay <captainstorage183@gmail.com>
2024-04-15 22:58:47 +0530

    Main

--- server/scripts/imagetotext.js
+++ server/scripts/imagetotext.js
@@ -1,51 +1,28 @@
+const fs = require('fs');
+
 async function uploadImage(imageData, promptext) {
-  try {
-      const url = `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_APP_ID}/ai/run/@cf/unum/uform-gen2-qwen-500m`;
+    try {
+        // Convert base64 image data to buffer
+        const imageBuffer = Buffer.from(imageData, 'base64');
 
-      const { default: fetch } = await import('node-fetch');
+        const url = `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_APP_ID}/ai/run/@cf/unum/uform-gen2-qwen-500m`;
+        const { default: fetch } = await import('node-fetch');
 
-      const inputSchema = JSON.stringify({
-          "oneOf": [
-              {
-                  "type": "string",
-                  "format": "binary"
-              },
-              {
-                  "type": "object",
-                  "properties": {
-                      "image": {
-                          "type": "array",
-                          "items": {
-                              "type": "number"
-                          }
-                      },
-                      "prompt": {
-                          "type": "string"
-                      },
-                      "max_tokens": {
-                          "type": "integer",
-                          "default": 512
-                      }
-                  }
-              }
-          ]
-      });
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ image: [...imageBuffer], prompt: promptext, max_tokens: 512 }),
+        });
 
-      const response = await fetch(url, {
-          method: 'POST',
-          headers: {
-              'Authorization': `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
-              'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ image: imageData, prompt: promptext, max_tokens: 512 }),
-      });
-
-      const responseData = await response.json();
-      return responseData.result.description;
-  } catch (error) {
-      console.error('Error uploading image:', error);
-      return null;
-  }
+        const responseData = await response.json();
+        return responseData.result.description;
+    } catch (error) {
+        console.error('Error uploading image:', error);
+        return null;
+    }
 }
 
 module.exports = { uploadImage };
